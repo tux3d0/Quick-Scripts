80ecc2961ca2d5cedbca7098bb193355231f06b1
Tuxnix <8356434+tux3d0@users.noreply.github.com>
2025-01-12 03:57:16 -0500

    Update sysInfo_basic.sh
    -Added Styling
    -Added More Basic System Info & Network Info

--- Scripts/sysInfo_basic.sh
+++ Scripts/sysInfo_basic.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
+display_message() {
+    local message=$1
+    local border="==============================================================================="
+    echo -e "\n$border$message$border\n"
+}
+
 function system_info() {
-    echo "Operating System: $(uname -o)"
-    echo "Kernel Version: $(uname -r)"
-    echo "Total RAM: $(free -h | grep Mem | awk '{print $2}')"
-    echo "Available RAM: $(free -h | grep Mem | awk '{print $7}')"
-    echo "Total Disk Space: $(df -h --total | grep total | awk '{print $2}')"
-    echo "Available Disk Space: $(df -h --total | grep total | awk '{print $4}')"
+    local msg="
+|                                                                             
+|           System Information :                                               
+|    ---------------------------------                                        
+|    Hostname: $(hostname)                                                    
+|    Uptime: $(uptime -p)                                                     
+|    Operating System: $(uname -o)                                            
+|    Kernel Version: $(uname -r)                                              
+|    CPU Architecture: $(uname -m)                                            
+|    Total RAM: $(free -h | grep Mem | awk '{print $2}')                      
+|    Available RAM: $(free -h | grep Mem | awk '{print $7}')                  
+|    Total Disk Space: $(df -h --total | grep total | awk '{print $2}')       
+|    Available Disk Space: $(df -h --total | grep total | awk '{print $4}')
+|
+|           Network Information :
+|    ----------------------------------
+|    Wifi SSID: $(iwgetid -r)
+|    Wifi Signal Strength: $(iwgetid -r | xargs -I {} iwconfig {} | grep Signal | awk '{print $4}' | cut -d= -f2) 
+|    Network Name: $(hostname -f)
+|    Domain Name: $(hostname -d) 
+|    Gateway: $(ip route | grep default | awk '{print $3}') 
+|    IPv4 Address: $(ip addr | grep inet | grep -v inet6 | awk '{print $2}' | paste -sd, -)
+|    IPv6 Address: $(ip addr | grep inet6 | grep -v ::1 | awk '{print $2}' | paste -sd, -)
+|    Ethernet MAC Address: $(ip link | grep ether | awk '{print $2}' | head -n 1)
+|    Wifi MAC Address: $(ip link | grep ether | awk '{print $2}' | tail -n 1)
+|    Subnet Mask: $(ifconfig | grep Mask | awk '{print $4}' | paste -sd, -)
+|    DNS Servers: $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}' | paste -sd, -)clear
+|    Open Ports: $(ss -tuln | grep LISTEN | awk '{print $4}' | cut -d: -f2 | sort -u | paste -sd, -)
+|     
+"
+    display_message "$msg"
 }
 
 system_info
